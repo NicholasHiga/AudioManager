e2dd46c59a777058b870100e044f6e449793580d
Nicholas Higa <nicholas_higa@hotmail.com>
2015-11-12 01:05:40 -0500

    Initial Build

---
+++ Bengine/AudioManager.cpp
@@ -0,0 +1,17 @@
+#include "AudioManager.h"
+
+AudioManager::AudioManager()
+{
+	/*alGetError();
+
+	ALCdevice * deviceAL = alcOpenDevice(NULL);
+
+	if (deviceAL == NULL)
+	{
+		LogError("Failed to init OpenAL device.");
+		return;
+	}
+
+	ALCcontext * contextAL = alcCreateContext(deviceAL, NULL);
+	AL_CHECK(alcMakeContextCurrent(contextAL));*/
+}
---
+++ Bengine/AudioManager.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <AL/al.h>
+#include <AL/alc.h>
+
+class AudioManager
+{
+public:
+	AudioManager();
+
+};
